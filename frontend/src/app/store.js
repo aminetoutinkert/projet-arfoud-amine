4ade5dfe18c926d1bfbe67dda03f62d56e7cddf2
bobmino <bob.mino@gmail.com>
2025-11-13 15:01:44 +0100

    feat(store): Intégration du articleSlice dans le Redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,10 +3,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 // Import du reducer d'authentification
 import authReducer from '../features/auth/authSlice'; // <-- NOUVEL IMPORT
+// 1. Importez le nouveau reducer des articles
+import articleReducer from '../features/articles/articleSlice'; // <-- NOUVEL IMPORT
 
 export const store = configureStore({
     reducer: {
         // Remplacement du reducer temporaire par le reducer d'authentification
         auth: authReducer, // <-- MODIFICATION MAJEURE
+        // 2. Associez le articleReducer au slice 'article'
+        article: articleReducer, // <-- NOUVELLE ENTREE
     },
 });
